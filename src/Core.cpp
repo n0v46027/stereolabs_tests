71c92f9c493052988f1ef44946587d6a503260c7
N0v4 <antoine.le@epitech.eu>
2024-11-29 18:13:41 +0100

    feat(matrix): store transfomr points in container

--- src/Core.cpp
+++ src/Core.cpp
@@ -15,17 +15,17 @@ void Core::transform_matrix(const std::array<std::array<double, MATRIX_SIZE>, MA
     for (auto &point: points) {
         for (int i = 0; i < MATRIX_SIZE; i++) {
             for (int j = 0; j < MATRIX_SIZE; j++) {
-                std::cout << point._points3D.at(j) << " " << matrix.at(i).at(j) << std::endl;
                 new_point._points3D.at(i) += point._points3D.at(j) * matrix.at(i).at(j);
             }
         }
-        std::cout << new_point._points3D.at(0) << " " << new_point._points3D.at(1) << " " << new_point._points3D.at(2) << " " << new_point._points3D.at(3) << std::endl;
+        transform_points.push_back(new_point);
         break;
     }
 }
 
 void Core::run() {
     transform_matrix(_parser.get_matrix().at(CLOUD_POINT_1), _parser.get_points().at(CLOUD_POINT_1), _transform_points.at(CLOUD_POINT_1));
+    transform_matrix(_parser.get_matrix().at(CLOUD_POINT_2), _parser.get_points().at(CLOUD_POINT_2), _transform_points.at(CLOUD_POINT_2));
 }
 
 Core::~Core() {
